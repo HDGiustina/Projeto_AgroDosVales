147f56822eac43c0b16dcc6c460d02b346acc667
HelenaDGiustina <helenadgiustina@gmail.com>
2025-10-13 00:55:03 -0300

    refactor: Refatorando o footer

--- src/components/layoutComponents/FooterBar.vue
+++ src/components/layoutComponents/FooterBar.vue
@@ -1,40 +1,39 @@
 <template>
-  <q-footer style="background-color: rgba(247, 245, 245, 0.726)" class="flex">
-      <q-toolbar class="q-ml-md q-mt-lg">
-        <q-toolbar-title class="q-mb-md">
-            <q-img src="../../assets/agroVales.png" fit="contain" width="200px" loading="eager"/>
+  <q-footer class="flex" style="background: white; display: grid;">
+      <q-toolbar class="q-my-lg footer-container">
+        <q-toolbar-title class="q-mb-md footer-logo">
+            <q-img src="../../assets/Logo_grande.png" fit="contain" loading="eager" class="footer-image"/>
         </q-toolbar-title>
+        <section class="row footer-main">
+          <div class="column col">
+            <h2 class="footer-title">Inova RS</h2>
+            <p class="footer-text">CEP: 90020-020</p>
+            <p class="footer-text">CNPJ: 52.624.053/0001-06</p>
+            <p class="footer-text">Av. Borges de Medeiros, 1.501, Praia de Belas</p>
+            <p class="footer-text">Centro Administrativo Fernando Ferrari, 18º andar</p>
+          </div>
+          <div class="column col">
+            <h2 class="footer-title">Sobre o Inova RS</h2>
+            <a class="footer-text" href="https://sict.rs.gov.br/quem-somos" target="_blank">Quem somos</a>
+            <a class="footer-text" href="https://sict.rs.gov.br/acesso-a-informacao" target="_blank">Acesso à informação</a>
+            <a class="footer-text" href="https://sict.rs.gov.br/protecao-de-dados-pessoais" target="_blank">Proteção de Dados Pessoais</a>
+          </div>
+          <div class="column col">
+            <h2 class="footer-title">Redes sociais</h2>
+            <div class="row">
+              <a class="footer-text" href="https://www.inova.rs.gov.br/conheca-inova-rs" target="_blank">Acompanhe o site do Inova Rs</a>
+            </div>
+          </div>
+          <div class="column col">
+            <h2 class="footer-title">Contate Inova</h2>
+            <div class="row">
+              <a class="footer-text" href="mailto:agrodosvales@gmail.com">agrodosvales@gmail.com</a>
+            </div>
+          </div >
+        </section>
       </q-toolbar>
-      <div class="row" style="display: flex; flex-wrap: wrap">
-        <div class="column col" style="min-width: 20vw">
-          <p style="color: #5A645B; font-weight: 600" class="q-ml-lg">Inova RS</p>
-          <p style="color: #5A645B; font-weight: 400" class="q-ml-lg">CEP: 90020-020</p>
-          <p style="color: #5A645B; font-weight: 400" class="q-ml-lg">CNPJ: 52.624.053/0001-06</p>
-          <p style="color: #5A645B; font-weight: 400" class="q-ml-lg">Av. Borges de Medeiros, 1.501, Praia de Belas</p>
-          <p style="color: #5A645B; font-weight: 400" class="q-ml-lg">Centro Administrativo Fernando Ferrari, 18º andar</p>
-        </div>
-        <div class="column col" style="min-width: 20vw">
-          <p style="font-weight: bold; color: #5A645B">Sobre o Inova RS</p>
-          <p style="color: #5A645B; font-weight: 400">Localização</p>
-          <p style="color: #5A645B; font-weight: 400">Quem somos</p>
-          <p style="color: #5A645B; font-weight: 400">Acesso à informação</p>
-          <p style="color: #5A645B; font-weight: 400">Proteção de Dados Pessoais</p>
-        </div>
-        <div class="column col" style="min-width: 20vw">
-          <p style="font-weight: bold; color: #5A645B">Nos siga nas redes sociais</p>
-          <div class="row">
-            <q-icon name="app:WebIcon" size="20px" class="q-mr-xs" color="black"/>
-            <a style="color: #5A645B; text-decoration: none; font-weight: 400; font-size: 16px;" href="https://www.inova.rs.gov.br/conheca-inova-rs" target="_blank">Acompanhe nosso site</a>
-          </div>
-        </div>
-        <div class="column col" style="min-width: 20vw">
-          <p style="font-weight: bold; color: #5A645B">Conatate Inova</p>
-          <div class="row">
-            <q-icon name="app:Email" size="20px" class="q-mr-xs" color="black"/>
-            <a style="color: #5A645B; text-decoration: none; font-weight: 400; font-size: 16px;" href="https://www.inovars/facebook" target="_blank">agrodosvales@gmail.com</a>
-          </div>
-        </div >
-      </div>
+      <hr class="footer-hr" color="#E5E7EB"/>
+      <p class="footer-copyright">© 2025 Agro dos Vales. Todos os direitos reservados.</p>
     </q-footer>
 </template>
 
@@ -44,3 +43,71 @@ export default defineComponent({
   name: 'FooterBar'
 })
 </script>
+
+<style scoped>
+
+.footer-logo {
+  display: flex;
+  justify-content: center;
+  flex: none;
+  width: 30%;
+}
+.footer-image{
+  max-width: 400px;
+}
+
+.footer-main {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 65%;
+  gap: 16px;
+}
+
+.footer-title {
+  font-weight: bold;
+  color: var(--color-dark);
+  font-size: 16px;
+  line-height: normal;
+}
+
+.footer-text {
+  color: #818181;
+  font-size: 14px;
+  margin: 0 0  10px;
+}
+
+.footer-hr {
+  width: 90%;
+  margin: 20px auto;
+}
+
+.footer-copyright {
+  color: var(--color-dark);
+  font-size: 14px;
+  margin: 0 0 20px 5%;
+}
+
+@media screen and (max-width: 1024px) {
+  .footer-container {
+    display: flex;
+    flex-direction: column;
+  }
+  .footer-main {
+    width: 90%;
+  }
+}
+@media screen and (max-width: 820px) {
+  .footer-main {
+    flex-direction: column;
+  }
+  .footer-logo {
+    width: 90%;
+  }
+}
+@media screen and (max-width: 420px) {
+  .footer-copyright, .footer-text  {
+    font-size: 12px;
+  }
+}
+</style>
